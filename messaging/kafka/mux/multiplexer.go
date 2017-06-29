9f337654c2539344479f07959a44fcd15142decf
Lukas Macko <lmacko@cisco.com>
2017-06-29 17:39:25 +0200

    messaging API + kafka impl

--- messaging/kafka/mux/multiplexer.go
+++ messaging/kafka/mux/multiplexer.go
@@ -2,8 +2,6 @@ package mux
 
 import (
 	"fmt"
-	"github.com/Shopify/sarama"
-	"github.com/golang/protobuf/proto"
 	"github.com/ligato/cn-infra/db/keyval"
 	lg "github.com/ligato/cn-infra/logging/logrus"
 	"github.com/ligato/cn-infra/messaging/kafka/client"
@@ -43,28 +41,6 @@ type Multiplexer struct {
 	// factory that crates consumer used in the Multiplexer
 	consumerFactory func(topics []string, groupId string) (*client.Consumer, error)
 	closeCh         chan struct{}
-}
-
-// Connection is an entity that provides access to shared producers/consumers of multiplexer.
-type Connection struct {
-	// multiplexer is used for access to kafka brokers
-	multiplexer *Multiplexer
-
-	// name identifies the connection
-	name string
-}
-
-// ProtoConnection is an entity that provides access to shared producers/consumers of multiplexer.
-// The value of message are marshaled and unmarshaled to/from proto.message behind the scene.
-type ProtoConnection struct {
-	// multiplexer is used for access to kafka brokers
-	multiplexer *Multiplexer
-
-	// name identifies the connection
-	name string
-
-	// serializer marshals and unmarshals data to/from proto.Message
-	serializer keyval.Serializer
 }
 
 // asyncMeta is auxiliary structure used by Multiplexer to distribute consumer messages
@@ -233,144 +209,4 @@ func (mux *Multiplexer) stopConsuming(topic string, name string) error {
 	}
 	delete(*subs, name)
 	return nil
-}
-
-// ConsumeTopic is called to start consuming of a topic.
-// Function can be called until the multiplexer is started, it returns an error otherwise.
-// The provided channel should be buffered, otherwise messages might be lost.
-func (conn *Connection) ConsumeTopic(msgChan chan *client.ConsumerMessage, topics ...string) error {
-	conn.multiplexer.rwlock.Lock()
-	defer conn.multiplexer.rwlock.Unlock()
-
-	if conn.multiplexer.started {
-		return fmt.Errorf("ConsumeTopic can be called only if the multiplexer has not been started yet")
-	}
-
-	for _, topic := range topics {
-		// check if we have already consumed the topic and partition
-		subs, found := conn.multiplexer.mapping[topic]
-
-		if !found {
-			subs = &map[string]chan *client.ConsumerMessage{}
-			conn.multiplexer.mapping[topic] = subs
-		}
-		// add subscription to consumerList
-		(*subs)[conn.name] = msgChan
-		conn.multiplexer.mapping[topic] = subs
-	}
-	return nil
-}
-
-// StopConsuming cancels the previously created subscription for consuming the topic.
-func (conn *Connection) StopConsuming(topic string) error {
-	return conn.multiplexer.stopConsuming(topic, conn.name)
-}
-
-// SendSyncByte sends a message that uses byte encoder using the sync API
-func (conn *Connection) SendSyncByte(topic string, key []byte, value []byte) (partition int32, offset int64, err error) {
-	return conn.SendSyncMessage(topic, sarama.ByteEncoder(key), sarama.ByteEncoder(value))
-}
-
-// SendSyncString sends a message that uses string encoder using the sync API
-func (conn *Connection) SendSyncString(topic string, key string, value string) (partition int32, offset int64, err error) {
-	return conn.SendSyncMessage(topic, sarama.StringEncoder(key), sarama.StringEncoder(value))
-}
-
-//SendSyncMessage sends a message using the sync API
-func (conn *Connection) SendSyncMessage(topic string, key client.Encoder, value client.Encoder) (partition int32, offset int64, err error) {
-	msg, err := conn.multiplexer.syncProducer.SendMsg(topic, key, value)
-	if err != nil {
-		return 0, 0, err
-	}
-	return msg.Partition, msg.Offset, err
-}
-
-// SendAsyncByte sends a message that uses byte encoder using the async API
-func (conn *Connection) SendAsyncByte(topic string, key []byte, value []byte, meta interface{}, successChan chan *client.ProducerMessage, errChan chan *client.ProducerError) {
-	conn.SendAsyncMessage(topic, sarama.ByteEncoder(key), sarama.ByteEncoder(value), meta, successChan, errChan)
-}
-
-// SendAsyncString sends a message that uses string encoder using the async API
-func (conn *Connection) SendAsyncString(topic string, key string, value string, meta interface{}, successChan chan *client.ProducerMessage, errChan chan *client.ProducerError) {
-	conn.SendAsyncMessage(topic, sarama.StringEncoder(key), sarama.StringEncoder(value), meta, successChan, errChan)
-}
-
-// SendAsyncMessage sends a message using the async API
-func (conn *Connection) SendAsyncMessage(topic string, key client.Encoder, value client.Encoder, meta interface{}, successChan chan *client.ProducerMessage, errChan chan *client.ProducerError) {
-	auxMeta := &asyncMeta{successChan: successChan, errorChan: errChan, usersMeta: meta}
-	conn.multiplexer.asyncProducer.SendMsg(topic, key, value, auxMeta)
-}
-
-// SendSyncMessage sends a message using the sync API
-func (conn *ProtoConnection) SendSyncMessage(topic string, key string, value proto.Message) (partition int32, offset int64, err error) {
-	data, err := conn.serializer.Marshal(value)
-	if err != nil {
-		return 0, 0, err
-	}
-	msg, err := conn.multiplexer.syncProducer.SendMsg(topic, sarama.StringEncoder(key), sarama.ByteEncoder(data))
-	if err != nil {
-		return 0, 0, err
-	}
-	return msg.Partition, msg.Offset, err
-}
-
-// SendAsyncMessage sends a message using the async API
-func (conn *ProtoConnection) SendAsyncMessage(topic string, key string, value proto.Message, meta interface{}, successChan chan *client.ProducerMessage, errChan chan *client.ProducerError) error {
-	data, err := conn.serializer.Marshal(value)
-	if err != nil {
-		return err
-	}
-	auxMeta := &asyncMeta{successChan: successChan, errorChan: errChan, usersMeta: meta}
-	conn.multiplexer.asyncProducer.SendMsg(topic, sarama.StringEncoder(key), sarama.ByteEncoder(data), auxMeta)
-	return nil
-}
-
-// ConsumeTopic is called to start consuming given topics.
-// Function can be called until the multiplexer is started, it returns an error otherwise.
-// The provided channel should be buffered, otherwise messages might be lost.
-func (conn *ProtoConnection) ConsumeTopic(msgChan chan *client.ProtoConsumerMessage, topics ...string) error {
-	conn.multiplexer.rwlock.Lock()
-	defer conn.multiplexer.rwlock.Unlock()
-
-	if conn.multiplexer.started {
-		return fmt.Errorf("ConsumeTopic can be called only if the multiplexer has not been started yet")
-	}
-
-	internalChannel := make(chan *client.ConsumerMessage)
-
-	go func() {
-	messageHandler:
-		for {
-			select {
-			case msg := <-internalChannel:
-				select {
-				case msgChan <- client.NewProtoConsumerMessage(msg, conn.serializer):
-				default:
-					log.Warn("Unable to deliver message to consumer")
-				}
-			case <-conn.multiplexer.closeCh:
-				break messageHandler
-			}
-		}
-		close(internalChannel)
-	}()
-
-	for _, topic := range topics {
-		// check if we have already consumed the topic and partition
-		subs, found := conn.multiplexer.mapping[topic]
-
-		if !found {
-			subs = &map[string]chan *client.ConsumerMessage{}
-			conn.multiplexer.mapping[topic] = subs
-		}
-		// add subscription to consumerList
-		(*subs)[conn.name] = internalChannel
-		conn.multiplexer.mapping[topic] = subs
-	}
-	return nil
-}
-
-// StopConsuming cancels the previously created subscription for consuming the topic.
-func (conn *ProtoConnection) StopConsuming(topic string) error {
-	return conn.multiplexer.stopConsuming(topic, conn.name)
 }
