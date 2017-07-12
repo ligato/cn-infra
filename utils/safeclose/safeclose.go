3c83d09340b41d32f639dce4e8a4b52d49fb05db
Lukas Macko <lmacko@cisco.com>
2017-07-12 15:53:29 +0200

    Use default logger in safeclose

--- utils/safeclose/safeclose.go
+++ utils/safeclose/safeclose.go
@@ -16,9 +16,9 @@ package safeclose
 
 import (
 	"errors"
-	log "github.com/ligato/cn-infra/logging/logrus"
 	"io"
 	"reflect"
+	"github.com/ligato/cn-infra/logging/logroot"
 )
 
 // CloserWithoutErr is similar interface to GoLang Closer but Close() does not return error
@@ -30,7 +30,7 @@ type CloserWithoutErr interface {
 func Close(obj interface{}) error {
 	defer func() {
 		if r := recover(); r != nil {
-			log.Error("Recovered in safeclose", r)
+			logroot.Logger().Error("Recovered in safeclose", r)
 		}
 	}()
 
@@ -46,7 +46,7 @@ func Close(obj interface{}) error {
 			}
 		} else if closer, ok := obj.(io.Closer); ok {
 			if closer != nil {
-				log.Debug("closer: ", closer)
+				logroot.Logger().Debug("closer: ", closer)
 				err := closer.Close()
 				return err
 			}
@@ -63,7 +63,7 @@ func Close(obj interface{}) error {
 func CloseAll(objs ...interface{}) (details []error, errOccured error) {
 	defer func() {
 		if r := recover(); r != nil {
-			log.Error("Recovered in safeclose", r)
+			logroot.Logger().Error("Recovered in safeclose", r)
 		}
 	}()
 
