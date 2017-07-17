231fceea3b966bd3d1da235829db75130d537f3c
jozef.slezak <jozef.slezak@pantheon.tech>
2017-07-17 14:34:22 +0200

    ODPM-419 introduce Expression interface (was string) + delete unit test

--- db/sql/cassandra/cassa_broker_impl.go
+++ db/sql/cassandra/cassa_broker_impl.go
@@ -16,8 +16,8 @@ package cassandra
 
 import (
 	r "reflect"
-	"gitlab.cisco.com/ctao/vnf-agent/examples/gocql/sql"
 	"github.com/willfaught/gockle"
+	"github.com/ligato/cn-infra/db/sql"
 )
 
 // NewBrokerUsingSession is a constructor. Use it like this:
@@ -42,100 +42,68 @@ type ValIterator struct {
 	Delegate gockle.Iterator
 }
 
-/*
-// NewTxn creates a new Data Broker transaction. A transaction can
-// hold multiple operations that are all committed to the data
-// store together. After a transaction has been created, one or
-// more operations (put or delete) can be added to the transaction
-// before it is committed.
-func (pdb *BrokerCassa) NewTxn() keyval.ProtoTxn {
-	return &Txn{txn: pdb.broker.NewTxn(), serializer: pdb.serializer}
+// ErrIterator is an iterator that stops immediately and just returns last error on Close()
+type ErrIterator struct {
+	LastError error
 }
-// Put writes the provided key-value item into the data store.
-//
-// Returns an error if the item could not be written, ok otherwise.
-func (pdb *BrokerCassa) Put(statement string, value interface{}, opts ...keyval.PutOption) error {
-	return errors.New("Not supported")
-}
-*/
 
-// Put writes the entity into the data store.
-// Returns an error if the item could not be written, ok otherwise.
-//
-// Example usage:
-//
-//    err = db.Put("ID='James Bond'", &User{"James Bond", "James", "Bond"})
-func (pdb *BrokerCassa) Put(where string, value interface{} /*TODO TTL, opts ...keyval.PutOption*/) error {
-	statement, _, err := sql.Update(r.ValueOf(value).Type().Name() /*TODO extract method / make customizable*/ ,
-		value                                                      /*, TODO TTL*/)
+// Put - see the description in interface sql.Broker.Put().
+// Put generates statement & binding for gocql Exec()
+func (pdb *BrokerCassa) Put(where sql.Expression, pointerToAStruct interface{} /*TODO TTL, opts ...keyval.PutOption*/) error {
+	statement, bindings, err := PutExpToString(where, pointerToAStruct)
 	if err != nil {
 		return err
 	}
-
-	return pdb.session.Exec(statement+" WHERE "+where, structFieldPtrs(value)...)//TODO prepared statement also in where
+	return pdb.session.Exec(statement, bindings)
 }
 
+// Exec - see the description in interface sql.Broker.ExecPut()
 // Exec runs statement (AS-IS) using gocql
 func (pdb *BrokerCassa) Exec(statement string, binding ...interface{}) error {
 	return pdb.session.Exec(statement, binding...)
 }
 
-// Delete removes from datastore values.
-func (pdb *BrokerCassa) Delete(fromWhere string) (existed bool, err error) {
-	/*	statement, _, err := sql.Delete(pdb.KeyNamespace, pdb.TableName, where)
-		if err != nil {
-			return err
-		}
-
-		fmt.Println("xxx statement ", statement)
-
-		// TODO stop using reflect.FieldsAndValues
-		fields, values, _ := reflect.FieldsAndValues(value) //note, that type is cached inside
-		return pdb.session.Exec(statement+" WHERE "+where, ptrs(value, values, fields)...)
-	*/
-	panic("not yet implemented")
+// Delete - see the description in interface sql.Broker.ExecPut()
+// Delete generates statement & binding for gocql Exec()
+func (pdb *BrokerCassa) Delete(fromWhere sql.Expression) error {
+	statement, bindings, err := ExpToString(fromWhere)
+	if err != nil {
+		return err
+	}
+	return pdb.session.Exec("DELETE"+statement, bindings)
 }
 
-// GetValue retrieves one key-value item from the datastore. The item
-// is identified by the provided key.
-//
-// If the item was found, its value is un-marshaled and placed in
-// the `reqObj` message buffer and the function returns found=true.
-// If the object was not found, the function returns found=false.
-// Function returns revision=revision of the latest modification
-// If an error was encountered, the function returns an error.
-func (pdb *BrokerCassa) GetValue(query string, reqObj interface{}) (found bool, err error) {
+// GetValue - see the description in interface sql.Broker.GetValue()
+// GetValue just iterate once for ListValues()
+func (pdb *BrokerCassa) GetValue(query sql.Expression, reqObj interface{}) (found bool, err error) {
 	it := pdb.ListValues(query)
 	stop := it.GetNext(reqObj)
 	return !stop, it.Close()
 }
 
 // ListValues retrieves an iterator for elements stored under the provided key.
-// ListValues runs query (AS-IS) using gocql.
-// Use utilities to:
-// - generate query string
-// - fill slice by values from iterator (SliceIt).
-//
-// Example usage:
-//
-// db := cassa.NewBrokerUsingSession(session)
-// query := sql.SelectFrom(UserTable) + sql.Where(sql.FieldEq(&UserTable.LastName, UserTable, "Bond"))
-// users := &[]User{}
-// err := sql.SliceIt(users, db.ListValues(query))
-//
-func (pdb *BrokerCassa) ListValues(statement string) *ValIterator {
-	it := pdb.session.ScanIterator(statement)
+// ListValues runs query (AS-IS) using gocql Scan Iterator.
+func (pdb *BrokerCassa) ListValues(query sql.Expression) sql.ValIterator {
+	queryStr, binding, err := SelectExpToString(query)
+	if err != nil {
+		return &ErrIterator{err}
+	}
+
+	it := pdb.session.ScanIterator(queryStr, binding)
 	return &ValIterator{it}
 }
 
 // GetNext returns the following item from the result set. If data was returned, found is set to true.
-func (it *ValIterator) GetNext(val interface{}) (stop bool) {
-	if m, ok := val.(map[string]interface{}); ok {
+// argument "outVal" can be:
+// - pointer to structure
+// - map
+func (it *ValIterator) GetNext(outVal interface{}) (stop bool) {
+	if m, ok := outVal.(map[string]interface{}); ok {
 		ok = it.Delegate.ScanMap(m)
 		return !ok //if not ok than stop
 	}
 
-	ptrs := structFieldPtrs(val)
+	ptrs := structFieldPtrs(outVal)
 	ok := it.Delegate.Scan(ptrs...)
 	return !ok //if not ok than stop
 }
@@ -167,10 +135,24 @@ func structFieldPtrs(val interface{}) []interface{} {
 			}
 		}
 	}
+
 	return ptrs
 }
 
 // Close the iterator. Not the error is important (may occure during marshalling/un-marshalling)
 func (it *ValIterator) Close() error {
 	return it.Delegate.Close()
+}
+
+// GetNext returns the following item from the result set. If data was returned, found is set to true.
+// argument "outVal" can be:
+// - pointer to structure
+// - map
+func (it *ErrIterator) GetNext(outVal interface{}) (stop bool) {
+	return true
+}
+
+// Close the iterator. Not the error is important (may occure during marshalling/un-marshalling)
+func (it *ErrIterator) Close() error {
+	return it.LastError
 }
