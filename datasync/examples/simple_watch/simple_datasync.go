9ac9fa89ab6d54f8fe196856fccf00908e5f9b91
jozef.slezak <jozef.slezak@pantheon.tech>
2017-08-11 14:10:01 +0200

    ODPM-361 Editorial changes of "datasync" package (README.md, doc.go)

---
+++ datasync/examples/simple_watch/simple_datasync.go
@@ -0,0 +1,86 @@
+package simple_watch
+
+import (
+	"context"
+
+	"os"
+
+	"time"
+
+	"github.com/ligato/cn-infra/core"
+	"github.com/ligato/cn-infra/datasync"
+	"github.com/ligato/cn-infra/flavors/etcdkafka"
+	"github.com/ligato/cn-infra/logging"
+	"github.com/ligato/cn-infra/logging/logroot"
+	"github.com/ligato/cn-infra/utils/safeclose"
+)
+
+func main() {
+	logroot.Logger().SetLevel(logging.DebugLevel)
+
+	f := etcdkafka.Flavor{}
+	//TODO inject PluginXY
+	agent := core.NewAgent(logroot.Logger(), 15*time.Second, f.Plugins()...)
+
+	err := core.EventLoopWithInterrupt(agent, nil)
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+// ExamplePlugin does watching
+type PluginXY struct {
+	Watcher   datasync.Watcher //Injected
+	Logger    logging.Logger
+	ParentCtx context.Context
+
+	dataChange chan datasync.ChangeEvent
+	dataResync chan datasync.ResyncEvent
+	cancel     context.CancelFunc
+}
+
+func (plugin *PluginXY) Init() (err error) {
+	// initialize channels & start go routins
+	plugin.dataChange = make(chan datasync.ChangeEvent, 100)
+	plugin.dataResync = make(chan datasync.ResyncEvent, 100)
+
+	// initiate context & cancel function (to stop go routine)
+	var ctx context.Context
+	if plugin.ParentCtx == nil {
+		ctx, plugin.cancel = context.WithCancel(context.Background())
+	} else {
+		ctx, plugin.cancel = context.WithCancel(plugin.ParentCtx)
+	}
+
+	go func() {
+		for {
+			select {
+			case dataChangeEvent := <-plugin.dataChange:
+				plugin.Logger.Debug(dataChangeEvent)
+			case dataResyncEvent := <-plugin.dataResync:
+				plugin.Logger.Debug(dataResyncEvent)
+			case <-ctx.Done():
+				// stop watching for notifications
+				return
+			}
+		}
+	}()
+
+	return nil
+}
+
+func (plugin *PluginXY) AfterInit() error {
+	// subscribe plugin.channel for watching data (to really receive the data)
+	plugin.Watcher.WatchData("watchingXY", plugin.dataChange, plugin.dataResync, "keysXY")
+
+	return nil
+}
+
+func (plugin *PluginXY) Close() error {
+	// cancel watching the channels
+	plugin.cancel()
+
+	// close all resources / channels
+	_, err := safeclose.CloseAll(plugin.dataChange, plugin.dataResync)
+	return err
+}
