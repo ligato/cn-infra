3ec668c8ed7b99408d10fc99e7ab00f144a3f4c9
martinfrk <stdstm@gmail.com>
2017-08-15 13:34:08 +0200

    Update doc.go
    minor edits (grammar & typos)

--- statuscheck/doc.go
+++ statuscheck/doc.go
@@ -12,17 +12,17 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package statuscheck provides the API for reporting status changes from plugins to the statuscheck plugin,
-// that exposes it via ETCD and HTTP.
+// The statuscheck package provides an API for reporting status changes from plugins to the statuscheck plugin,
+// which exposes them via etcd and HTTP.
 //
-// The API provides just two functions, one for registering the plugin for status change reporting and one
+// The API provides only two functions, one for registering the plugin for status change reporting and one
 // for reporting status changes.
 //
 // To register a plugin for providing status reports, use Register() function:
 //	statuscheck.Register(PluginID, probe)
 //
-// If probe is not nil, statuscheck will periodically probe the plugin state through the provided function,
-// otherwise it is expected that the plugin itself will report state updates through ReportStateChange():
+// If probe is not nil, statuscheck will periodically probe the plugin state through the provided function.
+// Otherwise, it is expected that the plugin itself will report state updates through ReportStateChange():
 //	statuscheck.ReportStateChange(PluginID, statuscheck.OK, nil)
 //
 // The default status of a plugin after registering is Init.
