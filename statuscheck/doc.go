0e8e7fcd9907840a90cbef1f02ea8324e4e4395c
Jan Medved <jmedved@cisco.com>
2017-08-15 22:09:34 -0700

    Editorial changes to doc.go files

--- statuscheck/doc.go
+++ statuscheck/doc.go
@@ -12,8 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package statuscheck provides the API for reporting status changes from plugins to the statuscheck plugin,
-// that exposes it via ETCD and HTTP.
+// Package statuscheck collects health status from other plugins through
+// a plugin status report API and exposes the collected overall health
+// status to the outside world via ETCD and a REST API.
 //
 // The API provides just two functions, one for registering the plugin for status change reporting and one
 // for reporting status changes.
