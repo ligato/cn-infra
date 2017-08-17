7776a3d32bd34028ede8cf9958ca0b4f441416f9
jozef.slezak <jozef.slezak@pantheon.tech>
2017-08-17 15:15:51 +0200

    ODPM-361 fix datasync.Put

--- db/keyval/etcdv3/etcdv3_suite_test.go
+++ db/keyval/etcdv3/etcdv3_suite_test.go
@@ -27,7 +27,6 @@ import (
 	"github.com/ligato/cn-infra/logging/logroot"
 	"github.com/onsi/gomega"
 	"golang.org/x/net/context"
-	"github.com/ligato/cn-infra/datasync"
 )
 
 var dataBroker *BytesConnectionEtcd
