cd32a22588bf8d304383f3b5ad4b40b231521028
Madhura Pundlik <mpundlik@cisco.com>
2017-08-25 01:58:49 -0700

    SPOPT-1495 adding cassandra_rest flavor

---
+++ flavors/cassandrarest/cassandra_rest_flavor.go
@@ -0,0 +1,52 @@
+// Copyright (c) 2017 Cisco and/or its affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package cassandrarest
+
+import (
+	"github.com/ligato/cn-infra/core"
+	"github.com/ligato/cn-infra/db/sql/cassandra"
+	"github.com/ligato/cn-infra/flavors/rpc"
+	"github.com/namsral/flag"
+)
+
+//init defines cassandra flags // TODO switch to viper to avoid global configuration
+func init() {
+	flag.String("cassandra-config", "cassandra.conf",
+		"Location of the Cassandra Client configuration file; also set via 'CASSANDRA_CONFIG' env variable.")
+}
+
+// FlavorCassandraRest glues together FlavorRPC plugins with:
+// - CASSANDRA (for using with API to interact with Cassandra database)
+type FlavorCassandraRest struct {
+	rpc.FlavorRPC
+	CASSANDRA cassandra.Plugin
+}
+
+// Inject sets object references
+func (f *FlavorCassandraRest) Inject() (allReadyInjected bool) {
+	if !f.FlavorRPC.Inject() {
+		return false
+	}
+
+	f.CASSANDRA.Deps.PluginInfraDeps = *f.InfraDeps("cassandra")
+
+	return true
+}
+
+// Plugins combines all Plugins in flavor to the list
+func (f *FlavorCassandraRest) Plugins() []*core.NamedPlugin {
+	f.Inject()
+	return core.ListPluginsInFlavor(f)
+}
