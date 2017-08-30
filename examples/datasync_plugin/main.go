28f8472354d25bd533a832ef9ba218fbca058e73
Vladimir Lavor <vlavor@cisco.com>
2017-08-30 13:51:08 +0200

    etcd/datasync example: removed timers, injected logger
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- examples/datasync_plugin/main.go
+++ examples/datasync_plugin/main.go
@@ -12,7 +12,6 @@ import (
 	"github.com/ligato/cn-infra/examples/model"
 	"github.com/ligato/cn-infra/flavors/local"
 	"github.com/ligato/cn-infra/flavors/localdeps"
-	"github.com/ligato/cn-infra/logging"
 	"github.com/ligato/cn-infra/logging/logroot"
 	"github.com/ligato/cn-infra/utils/safeclose"
 	"github.com/namsral/flag"
@@ -31,31 +30,19 @@ import (
  * Main *
  ********/
 
-var log logging.Logger
-
 // Main allows running Example Plugin as a statically linked binary with Agent Core Plugins. Close channel and plugins
 // required for the example are initialized. Agent is instantiated with ExampleFlavor
 func main() {
-	log = logroot.StandardLogger()
+	log := logroot.StandardLogger()
 	// Init close channel to stop the example
 	exampleFinished := make(chan struct{}, 1)
 
-	flavor := ExampleFlavor{}
+	flavor := ExampleFlavor{closeChan: &exampleFinished}
 
 	// Create new agent
 	agent := core.NewAgent(log, 15*time.Second, append(flavor.Plugins())...)
 
-	// End when the ETCD example is finished
-	go closeExample("etcd txn example finished", exampleFinished)
-
 	core.EventLoopWithInterrupt(agent, exampleFinished)
-}
-
-// Stop the agent with desired info message
-func closeExample(message string, closeChannel chan struct{}) {
-	time.Sleep(12 * time.Second)
-	log.Info(message)
-	closeChannel <- struct{}{}
 }
 
 /**********
@@ -80,6 +67,8 @@ type ExampleFlavor struct {
 	ETCDDataSync kvdbsync.Plugin
 	// Example plugin
 	DatasyncExample ExamplePlugin
+	// For example purposes, use channel when the example is finished
+	closeChan *chan struct{}
 
 	injected bool
 }
@@ -102,6 +91,7 @@ func (ef *ExampleFlavor) Inject() (allReadyInjected bool) {
 	ef.DatasyncExample.InfraDeps = *ef.FlavorLocal.InfraDeps("datasync-example")
 	ef.DatasyncExample.Publisher = &ef.ETCDDataSync
 	ef.DatasyncExample.Watcher = &ef.ETCDDataSync
+	ef.DatasyncExample.closeChannel = ef.closeChan
 
 	return true
 }
@@ -124,9 +114,12 @@ type ExamplePlugin struct {
 	resyncChannel chan datasync.ResyncEvent  // Channel used by the watcher for resync events
 	context       context.Context            // Used to cancel watching
 	watchDataReg  datasync.WatchRegistration // To subscribe on data change/resync events
+	// Fields below are used to properly finish the example
+	eventCounter uint8
+	closeChannel *chan struct{}
 }
 
-// Deps is here to group injected dependencies of plugin to not mix with other plugin fields
+// Deps is a helper struct which is grouping all dependencies injected to the plugin
 type Deps struct {
 	InfraDeps localdeps.PluginInfraDeps   // injected
 	Publisher datasync.KeyProtoValWriter  // injected - To write ETCD data
@@ -140,6 +133,9 @@ func (plugin *ExamplePlugin) Init() error {
 	plugin.resyncChannel = make(chan datasync.ResyncEvent)
 	plugin.changeChannel = make(chan datasync.ChangeEvent)
 	plugin.context = context.Background()
+
+	plugin.InfraDeps.Log.Info("Initialization of the custom plugin for the ETCD example is completed")
+
 	return nil
 }
 
@@ -156,9 +152,23 @@ func (plugin *ExamplePlugin) AfterInit() error {
 		return err
 	}
 
-	log.Info("Initialization of the custom plugin for the ETCD example is completed")
+	go plugin.closeExample()
 
 	return nil
+}
+
+func (plugin *ExamplePlugin) closeExample() {
+	for {
+		// Two events are expected for successful example completion
+		if plugin.eventCounter == 2 {
+			// Close the watcher
+			plugin.context.Done()
+			plugin.InfraDeps.Log.Infof("etcd/datasync example finished, sending shutdown ...")
+			// Close the example
+			*plugin.closeChannel <- struct{}{}
+			break
+		}
+	}
 }
 
 // Close is called by Agent Core when the Agent is shutting down. It is supposed to clean up resources that were
@@ -176,8 +186,9 @@ const etcdIndex string = "index"
 
 // KeyProtoValWriter creates a simple data, then demonstrates CRUD operations with ETCD
 func (plugin *ExamplePlugin) etcdPublisher() {
+	// Wait for the consumer to initialize
 	time.Sleep(3 * time.Second)
-	log.Print("KeyValPublisher started")
+	plugin.InfraDeps.Log.Print("KeyValPublisher started")
 
 	// Convert data to the generated proto format
 	exampleData := plugin.buildData("string1", 0, true)
@@ -185,11 +196,11 @@ func (plugin *ExamplePlugin) etcdPublisher() {
 	// PUT: examplePut demonstrates how to use the Data Broker Put() API to create (or update) a simple data
 	// structure into ETCD
 	label := etcdKeyPrefixLabel(plugin.InfraDeps.ServiceLabel.GetAgentLabel(), etcdIndex)
-	log.Infof("Write data to %v", label)
+	plugin.InfraDeps.Log.Infof("Write data to %v", label)
 	plugin.Publisher.Put(label, exampleData)
 
 	// Prepare different set of data
-	log.Infof("Update data at %v", label)
+	plugin.InfraDeps.Log.Infof("Update data at %v", label)
 	exampleData = plugin.buildData("string2", 1, false)
 
 	// UPDATE: Put() performs both create operations (if index does not exist) and update operations
@@ -214,11 +225,10 @@ func etcdKeyPrefixLabel(agentLabel string, index string) string {
 // Consumer (watcher) is subscribed to watch on data store changes. Change arrives via data change channel and
 // its key is parsed
 func (plugin *ExamplePlugin) consumer() {
-	log.Print("KeyValProtoWatcher started")
+	plugin.InfraDeps.Log.Print("KeyValProtoWatcher started")
 	for {
 		select {
 		case dataChng := <-plugin.changeChannel:
-			log.Print("event")
 			// If event arrives, the key is extracted and used together with the expected prefix to
 			// identify item
 			key := dataChng.GetKey()
@@ -227,18 +237,20 @@ func (plugin *ExamplePlugin) consumer() {
 				// The first return value is diff - boolean flag whether previous value exists or not
 				err := dataChng.GetValue(&value)
 				if err != nil {
-					log.Error(err)
+					plugin.InfraDeps.Log.Error(err)
 				}
 				diff, err := dataChng.GetPrevValue(&previousValue)
 				if err != nil {
-					log.Error(err)
+					plugin.InfraDeps.Log.Error(err)
 				}
-				log.Infof("Event arrived to etcd eventHandler, key %v, update: %v, change type: %v,",
+				plugin.InfraDeps.Log.Infof("Event arrived to etcd eventHandler, key %v, update: %v, change type: %v,",
 					dataChng.GetKey(), diff, dataChng.GetChangeType())
+				// Increase event counter (expecting two events)
+				plugin.eventCounter++
 			}
 			// Another strings.HasPrefix(key, etcd prefix) ...
 		case <-plugin.context.Done():
-			log.Warnf("Stop watching events")
+			plugin.InfraDeps.Log.Warnf("Stop watching events")
 		}
 	}
 }
@@ -246,14 +258,14 @@ func (plugin *ExamplePlugin) consumer() {
 // KeyValProtoWatcher is subscribed to data change channel and resync channel. ETCD watcher adapter is used for this purpose
 func (plugin *ExamplePlugin) subscribeWatcher() (err error) {
 	prefix := etcdKeyPrefix(plugin.InfraDeps.ServiceLabel.GetAgentLabel())
-	log.Infof("Prefix: %v", prefix)
+	plugin.InfraDeps.Log.Infof("Prefix: %v", prefix)
 	plugin.watchDataReg, err = plugin.Watcher.
 		Watch("Example etcd plugin", plugin.changeChannel, plugin.resyncChannel, prefix)
 	if err != nil {
 		return err
 	}
 
-	log.Info("KeyValProtoWatcher subscribed")
+	plugin.InfraDeps.Log.Info("KeyValProtoWatcher subscribed")
 
 	return nil
 }
