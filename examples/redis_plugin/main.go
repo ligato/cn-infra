af3ee3b8a6c0f7d464acd350ab20041210548cda
Madhura Pundlik <mpundlik@cisco.com>
2017-09-18 11:30:31 -0700

    SPOPT-1569, 1576 - resolving merge conflict

--- examples/redis_plugin/main.go
+++ examples/redis_plugin/main.go
@@ -4,10 +4,6 @@ import (
 	"time"
 
 	"github.com/ligato/cn-infra/core"
-	"github.com/ligato/cn-infra/datasync/kvdbsync"
-	"github.com/ligato/cn-infra/datasync/resync"
-	"github.com/ligato/cn-infra/db/keyval"
-	"github.com/ligato/cn-infra/flavors/local"
 	log "github.com/ligato/cn-infra/logging/logroot"
 	"github.com/namsral/flag"
 )
@@ -27,46 +23,7 @@ func main() {
 
 // Redis flag to load config
 func init() {
-	flag.String("redis-config", "redis.conf",
-		"Location of the redis configuration file")
-}
-
-// Inject sets object references
-func (ef *ExampleFlavor) Inject() (allReadyInjected bool) {
-	// Init local flavor
-	if ef.FlavorLocal == nil {
-		ef.FlavorLocal = &local.FlavorLocal{}
-	}
-	ef.FlavorLocal.Inject()
-	ef.Redis.Deps.PluginInfraDeps = *ef.InfraDeps("redis")
-	ef.ResyncOrch.Deps.PluginLogDeps = *ef.LogDeps("redis-resync")
-	InjectKVDBSync(&ef.RedisDataSync, &ef.Redis, ef.Redis.PluginName, ef.FlavorLocal, &ef.ResyncOrch)
-	ef.RedisExample.Deps.PluginLogDeps = *ef.FlavorLocal.LogDeps("redis-example")
-	ef.RedisExample.closeChannel = ef.closeChan
-
-	return true
-}
-
-// InjectKVDBSync helper to set object references
-func InjectKVDBSync(dbsync *kvdbsync.Plugin,
-	db keyval.KvProtoPlugin, dbPlugName core.PluginName, local *local.FlavorLocal, resync resync.Subscriber) {
-
-	dbsync.Deps.PluginLogDeps = *local.LogDeps(string(dbPlugName) + "-datasync")
-	dbsync.KvPlugin = db
-	dbsync.ResyncOrch = resync
-	if local != nil {
-		dbsync.ServiceLabel = &local.ServiceLabel
-
-		if local.StatusCheck.Transport == nil {
-			local.StatusCheck.Transport = dbsync
-		}
-	}
-}
-
-// Plugins combines all Plugins in flavor to the list
-func (ef *ExampleFlavor) Plugins() []*core.NamedPlugin {
-	ef.Inject()
-	return core.ListPluginsInFlavor(ef)
+	flag.String("redis-config", "redis.conf","Location of the redis configuration file")
 }
 
 // ExamplePlugin to depict the use of Redis flavor
