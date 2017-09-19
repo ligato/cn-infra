e99c6ec34823081ecfb0c4870a97a2494ce77756
Vladimir Lavor <vlavor@cisco.com>
2017-09-19 16:40:11 +0200

    replaced kafka plugin dependency with mux
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- examples/kafka-plugin/manual-partitioner/deps.go
+++ examples/kafka-plugin/manual-partitioner/deps.go
@@ -4,12 +4,14 @@ import (
 	"github.com/ligato/cn-infra/core"
 	"github.com/ligato/cn-infra/flavors/local"
 	"github.com/ligato/cn-infra/messaging/kafka"
+	"github.com/ligato/cn-infra/messaging"
+	"github.com/ligato/cn-infra/flavors/connectors"
 )
 
 // Deps lists dependencies of ExamplePlugin.
 type Deps struct {
-	Kafka *kafka.Plugin // injected
-	local.PluginLogDeps // injected
+	Kafka               messaging.Mux // injected
+	local.PluginLogDeps               // injected
 }
 
 // ExampleFlavor is a set of plugins required for the example.
