0f68516ffa0fe0571c14bd3e62770bf0f4b69ad2
Madhura Pundlik <mpundlik@cisco.com>
2017-09-26 00:50:08 -0700

    SPOPT-1621 - fixing merge conflict removed unit tests for OR predicate since not valid for Cassandra

--- health/probe/plugin_impl_prometheus.go
+++ health/probe/plugin_impl_prometheus.go
@@ -77,10 +77,6 @@ type PrometheusPlugin struct {
 // Init may create a new (custom) instance of HTTP if the injected instance uses
 // different HTTP port than requested.
 func (p *PrometheusPlugin) Init() (err error) {
-	serviceLabel := p.String()
-	if p.Deps.ServiceLabel != nil {
-		serviceLabel = p.Deps.ServiceLabel.GetAgentLabel()
-	}
 
 	p.healthRegistry = prometheus.NewRegistry()
 
@@ -89,7 +85,7 @@ func (p *PrometheusPlugin) Init() (err error) {
 		Subsystem,
 		ServiceHealthName,
 		ServiceHealthHelp,
-		prometheus.Labels{ServiceLabel: serviceLabel},
+		prometheus.Labels{ServiceLabel: p.getServiceLabel()},
 		p.getServiceHealth,
 	)
 
@@ -100,7 +96,7 @@ func (p *PrometheusPlugin) Init() (err error) {
 		ServiceInfoName,
 		ServiceInfoHelp,
 		prometheus.Labels{
-			ServiceLabel:      serviceLabel,
+			ServiceLabel:      p.getServiceLabel(),
 			BuildVersionLabel: agentStatus.BuildVersion,
 			BuildDateLabel:    agentStatus.BuildDate},
 		func() float64 { return 1 },
@@ -281,4 +277,12 @@ func (p *PrometheusPlugin) String() string {
 		return string(p.PluginName)
 	}
 	return defaultPluginName
+}
+
+func (p *PrometheusPlugin) getServiceLabel() string {
+	serviceLabel := p.String()
+	if p.Deps.ServiceLabel != nil {
+		serviceLabel = p.Deps.ServiceLabel.GetAgentLabel()
+	}
+	return serviceLabel
 }
