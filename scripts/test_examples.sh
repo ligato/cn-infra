372722781442c0bc511988439621553ceb972acf
Stanislav Chlebec <stanislav.chlebec@pantheon.tech>
2017-10-11 09:47:53 +0200

    Commented redis test ...
    Added initialization for variable processedLines

--- scripts/test_examples.sh
+++ scripts/test_examples.sh
@@ -3,6 +3,7 @@
 TMP_FILE="/tmp/out"
 TMP_FILE2="/tmp/unprocessed"
 exitCode=0
+processedLines=0 
 PREV_IFS="$IFS"
 
 # test whether output of the command contains expected lines
@@ -181,45 +182,45 @@ stopCassandra
 
 #### Redis ###########################################################
 
-startRedis
-
-expected=("config: redis.NodeConfig:
-INFO[0004] GetValue(key1) = true ; val = val 1 ; revision = 0  loc=\"simple/simple.go(208)\" logger=defaultLogger tag=00000000
-INFO[0004] GetValue(key2) = true ; val = val 2 ; revision = 0  loc=\"simple/simple.go(208)\" logger=defaultLogger tag=00000000
-==> NOTE: key3 should have expired
-INFO[0004] GetValue(key3) = false                        loc=\"simple/simple.go(210)\" logger=defaultLogger tag=00000000
-==> NOTE: get(key) should return false
-INFO[0004] GetValue(key) = false                         loc=\"simple/simple.go(210)\" logger=defaultLogger tag=00000000
-INFO[0004] ListKeys(key):  key2 (rev 0)                  loc=\"simple/simple.go(228)\" logger=defaultLogger tag=00000000
-INFO[0004] ListKeys(key):  key1 (rev 0)                  loc=\"simple/simple.go(228)\" logger=defaultLogger tag=00000000
-INFO[0004] ListKeys(key): count = 2                      loc=\"simple/simple.go(231)\" logger=defaultLogger tag=00000000
-INFO[0004] ListValues(key):  key2 = val 2 (rev 0)        loc=\"simple/simple.go(249)\" logger=defaultLogger tag=00000000
-INFO[0004] ListValues(key):  key1 = val 1 (rev 0)        loc=\"simple/simple.go(249)\" logger=defaultLogger tag=00000000
-INFO[0004] ListValues(key): count = 2                    loc=\"simple/simple.go(252)\" logger=defaultLogger tag=00000000
-INFO[0004] doKeyInterator(): Expected 100 keys; Found 100  loc=\"simple/simple.go(277)\" logger=defaultLogger tag=00000000
-INFO[0004] doKeyValInterator(): Expected 100 keyVals; Found 100  loc=\"simple/simple.go(315)\" logger=defaultLogger tag=00000000
-INFO[0004] Delete(key): found = true                     loc=\"simple/simple.go(341)\" logger=defaultLogger tag=00000000
-==> NOTE: All keys should have been deleted
-INFO[0004] GetValue(key1) = false                        loc=\"simple/simple.go(210)\" logger=defaultLogger tag=00000000
-INFO[0004] GetValue(key2) = false                        loc=\"simple/simple.go(210)\" logger=defaultLogger tag=00000000
-INFO[0004] ListKeys(key): count = 0                      loc=\"simple/simple.go(231)\" logger=defaultLogger tag=00000000
-INFO[0004] ListValues(key): count = 0                    loc=\"simple/simple.go(252)\" logger=defaultLogger tag=00000000
-INFO[0004] txn(): keys = [key101 key102 key103 key104]   loc=\"simple/simple.go(353)\" logger=defaultLogger tag=00000000
-INFO[0004] ListValues(key):  key102 = 2 (rev 0)          loc=\"simple/simple.go(249)\" logger=defaultLogger tag=00000000
-INFO[0004] ListValues(key):  key104 = 4 (rev 0)          loc=\"simple/simple.go(249)\" logger=defaultLogger tag=00000000
-INFO[0004] ListValues(key):  key103 = 3 (rev 0)          loc=\"simple/simple.go(249)\" logger=defaultLogger tag=00000000
-INFO[0004] ListValues(key): count = 3                    loc=\"simple/simple.go(252)\" logger=defaultLogger tag=00000000
-INFO[0004] Sleep for 5 seconds                           loc=\"simple/simple.go(175)\" logger=defaultLogger tag=00000000
-INFO[0009] Closing connection                            loc=\"simple/simple.go(179)\" logger=defaultLogger tag=00000000
-==> NOTE: Call on a closed connection should fail.
-ERRO[0009] Delete(key) called on a closed connection     loc=\"simple/simple.go(338)\" logger=defaultLogger tag=00000000
-INFO[0009] Sleep for 10 seconds                          loc=\"simple/simple.go(186)\" logger=defaultLogger tag=00000000
-")
-
-cmd="examples/redis-lib/simple/simple -redis-config=node-client.yaml "
-testOutput "${cmd}" "${expected}"
-
-stopRedis
+# startRedis
+# 
+# expected=("config: redis.NodeConfig:
+# INFO[0004] GetValue(key1) = true ; val = val 1 ; revision = 0  loc=\"simple/simple.go(208)\" logger=defaultLogger tag=00000000
+# INFO[0004] GetValue(key2) = true ; val = val 2 ; revision = 0  loc=\"simple/simple.go(208)\" logger=defaultLogger tag=00000000
+# ==> NOTE: key3 should have expired
+# INFO[0004] GetValue(key3) = false                        loc=\"simple/simple.go(210)\" logger=defaultLogger tag=00000000
+# ==> NOTE: get(key) should return false
+# INFO[0004] GetValue(key) = false                         loc=\"simple/simple.go(210)\" logger=defaultLogger tag=00000000
+# INFO[0004] ListKeys(key):  key2 (rev 0)                  loc=\"simple/simple.go(228)\" logger=defaultLogger tag=00000000
+# INFO[0004] ListKeys(key):  key1 (rev 0)                  loc=\"simple/simple.go(228)\" logger=defaultLogger tag=00000000
+# INFO[0004] ListKeys(key): count = 2                      loc=\"simple/simple.go(231)\" logger=defaultLogger tag=00000000
+# INFO[0004] ListValues(key):  key2 = val 2 (rev 0)        loc=\"simple/simple.go(249)\" logger=defaultLogger tag=00000000
+# INFO[0004] ListValues(key):  key1 = val 1 (rev 0)        loc=\"simple/simple.go(249)\" logger=defaultLogger tag=00000000
+# INFO[0004] ListValues(key): count = 2                    loc=\"simple/simple.go(252)\" logger=defaultLogger tag=00000000
+# INFO[0004] doKeyInterator(): Expected 100 keys; Found 100  loc=\"simple/simple.go(277)\" logger=defaultLogger tag=00000000
+# INFO[0004] doKeyValInterator(): Expected 100 keyVals; Found 100  loc=\"simple/simple.go(315)\" logger=defaultLogger tag=00000000
+# INFO[0004] Delete(key): found = true                     loc=\"simple/simple.go(341)\" logger=defaultLogger tag=00000000
+# ==> NOTE: All keys should have been deleted
+# INFO[0004] GetValue(key1) = false                        loc=\"simple/simple.go(210)\" logger=defaultLogger tag=00000000
+# INFO[0004] GetValue(key2) = false                        loc=\"simple/simple.go(210)\" logger=defaultLogger tag=00000000
+# INFO[0004] ListKeys(key): count = 0                      loc=\"simple/simple.go(231)\" logger=defaultLogger tag=00000000
+# INFO[0004] ListValues(key): count = 0                    loc=\"simple/simple.go(252)\" logger=defaultLogger tag=00000000
+# INFO[0004] txn(): keys = [key101 key102 key103 key104]   loc=\"simple/simple.go(353)\" logger=defaultLogger tag=00000000
+# INFO[0004] ListValues(key):  key102 = 2 (rev 0)          loc=\"simple/simple.go(249)\" logger=defaultLogger tag=00000000
+# INFO[0004] ListValues(key):  key104 = 4 (rev 0)          loc=\"simple/simple.go(249)\" logger=defaultLogger tag=00000000
+# INFO[0004] ListValues(key):  key103 = 3 (rev 0)          loc=\"simple/simple.go(249)\" logger=defaultLogger tag=00000000
+# INFO[0004] ListValues(key): count = 3                    loc=\"simple/simple.go(252)\" logger=defaultLogger tag=00000000
+# INFO[0004] Sleep for 5 seconds                           loc=\"simple/simple.go(175)\" logger=defaultLogger tag=00000000
+# INFO[0009] Closing connection                            loc=\"simple/simple.go(179)\" logger=defaultLogger tag=00000000
+# ==> NOTE: Call on a closed connection should fail.
+# ERRO[0009] Delete(key) called on a closed connection     loc=\"simple/simple.go(338)\" logger=defaultLogger tag=00000000
+# INFO[0009] Sleep for 10 seconds                          loc=\"simple/simple.go(186)\" logger=defaultLogger tag=00000000
+# ")
+# 
+# cmd="examples/redis-lib/simple/simple -redis-config=node-client.yaml "
+# testOutput "${cmd}" "${expected}"
+# 
+# stopRedis
 
 #### Configs #############################################################
 
