0a202791e009cbce4020ee7fc8c59326b12a9f1b
jozef.slezak <jozef.slezak@pantheon.tech>
2017-10-13 11:44:25 +0200

    ODPM-641 examples now use new utils local/rpc - fix import

--- examples/redis-plugin/deps.go
+++ examples/redis-plugin/deps.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/ligato/cn-infra.bak20170821c/datasync"
+	"github.com/ligato/cn-infra/datasync"
 	"github.com/ligato/cn-infra/db/keyval"
 	"github.com/ligato/cn-infra/flavors/local"
 )
 
 // Deps is a helper struct which is grouping all dependencies injected to the plugin
 type Deps struct {
-	local.PluginLogDeps          // injected
-	Watcher datasync.Watcher     // injected
-	DB      keyval.KvProtoPlugin // injected
+	local.PluginLogDeps                 // injected
+	Watcher datasync.KeyValProtoWatcher // injected
+	DB      keyval.KvProtoPlugin        // injected
 }
