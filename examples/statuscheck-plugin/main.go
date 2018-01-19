9f2341b82444d4aa681ce2274bebc296b78b684c
Jan Medved <jmedved@cisco.com>
2018-01-19 14:51:49 -0800

    Added file headers to go files, removed deps.go, changed etcd prot in etcd.conf to 22379

--- examples/statuscheck-plugin/main.go
+++ examples/statuscheck-plugin/main.go
@@ -1,6 +1,21 @@
+// Copyright (c) 2018 Cisco and/or its affiliates.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
+	"github.com/ligato/cn-infra/health/statuscheck"
 	"time"
 
 	"github.com/ligato/cn-infra/core"
@@ -33,8 +48,8 @@ func main() {
 		connectors.InjectKVDBSync(etcdDataSync, etcdPlug, etcdPlug.PluginName, flavor, resyncOrch)
 
 		examplePlug := &ExamplePlugin{closeChannel: exampleFinished}
-		examplePlug.Deps.PluginInfraDeps = *flavor.InfraDeps("statuscheck-example")
-		examplePlug.Deps.StatusMonitor = &flavor.StatusCheck // Inject status check
+		examplePlug.PluginInfraDeps = *flavor.InfraDeps("statuscheck-example")
+		examplePlug.StatusMonitor = &flavor.StatusCheck // Inject status check
 
 		return []*core.NamedPlugin{
 			{etcdPlug.PluginName, etcdPlug},
@@ -47,7 +62,8 @@ func main() {
 
 // ExamplePlugin demonstrates the usage of datasync API.
 type ExamplePlugin struct {
-	Deps
+	local.PluginInfraDeps // injected
+	StatusMonitor         statuscheck.StatusReader
 
 	// Fields below are used to properly finish the example.
 	closeChannel chan struct{}
