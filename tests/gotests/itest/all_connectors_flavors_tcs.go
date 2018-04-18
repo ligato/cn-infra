48f8eca482e975619e82a15f676b679c4bde6f36
Vladimir Lavor <vlavor@cisco.com>
2018-04-18 03:22:30 +0200

    detach etcd plugin from skeleton plugin
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- tests/gotests/itest/all_connectors_flavors_tcs.go
+++ tests/gotests/itest/all_connectors_flavors_tcs.go
@@ -21,10 +21,6 @@ import (
 	"github.com/ligato/cn-infra/flavors/connectors"
 	"github.com/onsi/gomega"
 
-	"github.com/ligato/cn-infra/db/keyval/etcdv3"
-	etcdmock "github.com/ligato/cn-infra/db/keyval/etcdv3/mocks"
-	"github.com/ligato/cn-infra/flavors/local"
-	"github.com/ligato/cn-infra/messaging/kafka"
 	kafkamux "github.com/ligato/cn-infra/messaging/kafka/mux"
 )
 
@@ -54,39 +50,3 @@ func (t *suiteFlavorAllConnectors) TC01StartStopWithoutConfig() {
 
 	gomega.Expect(t.agent).ShouldNot(gomega.BeNil(), "agent is not initialized")
 }
-
-// MockAllConnectorsFlavor initializes embeded ETCD & Kafka MOCK
-//
-// Example:
-//
-//     kafkamock, _, _ := kafkamux.Mock(t)
-//     MockAllConnectorsFlavor(t, localFlavor)
-func MockAllConnectorsFlavor(t *testing.T, flavorLocal *local.FlavorLocal) (*connectors.AllConnectorsFlavor, *AllConnectorsFlavorMocks) {
-	kafkaMock := kafkamux.Mock(t)
-
-	embededEtcd := etcdmock.Embedded{}
-	embededEtcd.Start(t)
-	defer embededEtcd.Stop()
-
-	etcdClientLogger := flavorLocal.LoggerFor("emedEtcdClient")
-	etcdBytesCon, err := etcdv3.NewEtcdConnectionUsingClient(embededEtcd.Client(), etcdClientLogger)
-	if err != nil {
-		panic(err)
-	}
-
-	return &connectors.AllConnectorsFlavor{
-		ETCD:  *etcdv3.FromExistingConnection(etcdBytesCon, &flavorLocal.ServiceLabel),
-		Kafka: *kafka.FromExistingMux(kafkaMock.Mux),
-	}, &AllConnectorsFlavorMocks{kafkaMock}
-}
-
-/* TODO
-// TC02 asserts that injection works fine and agent starts & stops
-func (t *suiteFlavorAllConnectors) TC02StartStopMocks() {
-	flavor, _ := MockAllConnectorsFlavor(t.T, localFlavor)
-	t.Setup(flavor, t.T)
-	defer t.Teardown()
-
-	gomega.Expect(t.agent).ShouldNot(gomega.BeNil(), "agent is not initialized")
-}
-*/
