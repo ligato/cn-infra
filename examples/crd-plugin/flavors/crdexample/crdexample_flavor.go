6840cc7bd26d699959d4e41febbd8676a0bad8d5
Kyle Mestery <mestery@mestery.com>
2018-06-14 08:29:22 -0500

    Fix linter problem
    Signed-off-by: Kyle Mestery <mestery@mestery.com>

--- examples/crd-plugin/flavors/crdexample/crdexample_flavor.go
+++ examples/crd-plugin/flavors/crdexample/crdexample_flavor.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package netmesh defines flavor used for the netmesh agent.
+// Package crdexample defines flavor used for the netmesh agent.
 package crdexample
 
 import (
