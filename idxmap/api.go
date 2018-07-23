603e7c73d7f6925ce6647a1c16c56b98bf04cccb
Milan Lenco <milan.lenco@pantheon.tech>
2018-07-23 16:24:01 +0200

    Use end-of-the-line comment, not inside of type declaration.
    Signed-off-by: Milan Lenco <milan.lenco@pantheon.tech>

--- idxmap/api.go
+++ idxmap/api.go
@@ -38,7 +38,7 @@ type NamedMapping interface {
 
 	// ListFields returns a map of fields (secondary indexes) and their values
 	// currently associated with the item identified by <name>.
-	ListFields(name string) map[string] /* field */ []string /* values */
+	ListFields(name string) map[string][]string // field -> values
 
 	// Watch subscribes to receive notifications about the changes in the
 	// mapping. To receive changes through a channel, ToChan utility can be used.
