9d2107b0e8b15d617f18be0fac5234f3e9692ddf
Andrii <a.zakurenyi@gmail.com>
2019-10-07 19:18:14 +0300

    feat: Support string duration format in config files (#408)
    * Create new type to use when you need duration in config
    
    * Support new duration format without custom type
    
    * Compare types, not names

---
+++ config/parser_test.go
@@ -0,0 +1,54 @@
+package config
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/onsi/gomega"
+)
+
+func TestParseConfigFromYamlBytes(t *testing.T) {
+	RegisterTestingT(t)
+
+	type Duration string
+
+	type BigConfig struct {
+		Simple     string
+		Str        string `json:"name"`
+		Integer16  int16
+		Timeout    time.Duration
+		NotTimeout Duration
+	}
+
+	var testData = map[string]struct {
+		input string
+		want  BigConfig
+		fail  bool
+	}{
+		"simple":          {"simple: test", BigConfig{Simple: "test"}, false},
+		"bad simple":      {"simple test", BigConfig{}, true},
+		"json tag":        {"name: BigName", BigConfig{Str: "BigName"}, false},
+		"int16":           {"integer16: 25", BigConfig{Integer16: 25}, false},
+		"duration number": {"timeout: 5000000000", BigConfig{Timeout: 5 * time.Second}, false},
+		"duration string": {"timeout: 5s", BigConfig{Timeout: 5 * time.Second}, false},
+		"bad duration":    {"timeout: s5s", BigConfig{}, true},
+		"not duration":    {"nottimeout: 30SecToAgent", BigConfig{NotTimeout: "30SecToAgent"}, false},
+	}
+
+	for name, tt := range testData {
+		t.Run(name, func(t *testing.T) {
+			RegisterTestingT(t)
+
+			out := BigConfig{}
+			err := parseConfigFromYamlBytes([]byte(tt.input), &out)
+
+			if tt.fail {
+				Expect(err).To(HaveOccurred())
+				return
+			}
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(out).To(Equal(tt.want))
+		})
+	}
+}
