42c7431fdca1499e545baffc4305fa563f4ece65
Vladimir Lavor <vlavor@cisco.com>
2019-10-30 09:14:11 +0100

    feat: enable grpc plugin logging (#413)
    * Enable grpc plugin logging
    
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>
    
    * updated example, readme and deps
    
    Signed-off-by: Vladimir Lavor <vlavor@cisco.com>

--- examples/grpc-plugin/grpc-server/main.go
+++ examples/grpc-plugin/grpc-server/main.go
@@ -25,8 +25,10 @@ const PluginName = "myPlugin"
 func main() {
 	grpcPlug := grpc.NewPlugin(
 		grpc.UseHTTP(&rest.DefaultPlugin),
+		// Remove 'UseConf' in order to allow GRPC config file
 		grpc.UseConf(grpc.Config{
 			Endpoint: "localhost:9111",
+			ExtendedLogging: true,
 		}),
 		grpc.UseAuth(&grpc.Authenticator{
 			Username: "testuser",
